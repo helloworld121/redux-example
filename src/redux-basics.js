9e148a9be5b00f68a0dffd4685cb8998f43400bb
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-01-22 17:40:57 +0100

    initialize state with a default state

--- src/redux-basics.js
+++ src/redux-basics.js
@@ -1,12 +1,20 @@
 // import redux
 const redux = require('redux');
 
+// init the initial state, that will be used to init the store
+const initialState = {
+    counter: 0
+};
+
+
 // createStore is a function but I don't want to execute it yet
 const createStore = redux.createStore;
 
 // Reducer
 // a reducer always retrieves two arguments: the state and the action
-const rootReducer = (state, action) => {
+// to init the state with the default value
+//   => we pass it using js (ES6) syntax to init state with a default value
+const rootReducer = (state = initialState, action) => {
     // this reducer just returns the old state
     return state;
 }
